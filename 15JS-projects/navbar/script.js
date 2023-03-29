8c81c9746dd2c74b00a9d18df320ff50dd878b20
Julia Scherbina <juliascherbina15@gmail.com>
2023-03-29 21:39:45 +0300

    process

---
+++ 15JS-projects/navbar/script.js
@@ -0,0 +1,9 @@
+const links=document.querySelector('.links');
+const navToggle=document.querySelector('.nav-toggle');
+console.log(navToggle)
+console.log(links.classList)
+navToggle.addEventListener('click', function(){
+    console.log(links.classList);
+  alert('lfjdnkdjfn')
+})
+
