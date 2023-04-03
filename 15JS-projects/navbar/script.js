2aaf7dcb17f21f0b4b1e177cf316c561ecd27625
Julia Scherbina <juliascherbina15@gmail.com>
2023-04-03 17:28:36 +0300

    final

--- 15JS-projects/navbar/script.js
+++ 15JS-projects/navbar/script.js
@@ -3,12 +3,13 @@ const navToggle = document.querySelector('.nav-toggle');
 
 navToggle.addEventListener('click', function () {
   console.log(links.classList.contains('root'));
-  if (links.classList.contains('show-links')) {
-    links.classList.remove('show-links')
-  }
-  else {
-    links.classList.add('showlinks')
-  }
+  console.log(links.classList.contains('links'));
+  // if (links.classList.contains('show-links')) {
+  //   links.classList.remove('show-links')
+  // }
+  // else {
+  //   links.classList.add('show-links')
+  // }
   links.classList.toggle("show-links");
 })
 
