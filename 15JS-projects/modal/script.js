03df6812a125550f77465639dd2118ce2feefb83
Julia Scherbina <juliascherbina15@gmail.com>
2023-04-14 21:44:29 +0300

    finally final

--- 15JS-projects/modal/script.js
+++ 15JS-projects/modal/script.js
@@ -1,11 +1,17 @@
 const modalBtn = document.querySelector('.btn')
-const modalOverlay = document.querySelector('.modal-content')
+const modalOverlay = document.querySelector('.modal-overlay')
 const closeBtn = document.querySelector('.close-btn')
-
 modalBtn.addEventListener('click', function () {
     
-modalOverlay.classList.add('modal-open')
+modalOverlay.classList.add('modal-open');
+
 })
 closeBtn.addEventListener('click', function(){
     modalOverlay.classList.remove('modal-open')
+})
+
+
+
+modalBtn.addEventListener('click', function(){
+    
 })
