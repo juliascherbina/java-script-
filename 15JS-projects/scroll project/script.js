b280aad5c29cfc84a05ebc6612fd4100790778b4
Julia Scherbina <juliascherbina15@gmail.com>
2023-05-03 23:19:15 +0300

    in process

---
+++ 15JS-projects/scroll project/script.js
@@ -0,0 +1,22 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+const date=document.getElementById('date');
+date.innerHTML=new Date().getFullYear();
+// ********** close links ************
+const linksContainer=document.querySelector('.links-container')
+const navtoggle=document.querySelector('.nav-toggle')
+const links=document.querySelector('.links')
+navtoggle.addEventListener('click', function(){
+    linksContainer.classList.toggle('show-links')
+    // linksContainer.style.height='200px'
+    const heightOfContainer=linksContainer.getBoundingClientRect()
+    
+})
+// ********** fixed navbar ************
+
+// ********** smooth scroll ************
+// select links
